52ce0611ebc0d7c6c14659d8b8dc8ecbe9612cb5
Cloyd Nino Catanaoan <antimone2001@hotmail.com>
2018-03-11 18:42:42 -0500

    Added protection against WiFi disconnection

--- Objects/src/b4r_main.cpp
+++ Objects/src/b4r_main.cpp
@@ -61,157 +61,162 @@ void b4r_main::_connecttowifi(){
 const UInt cp = B4R::StackMemory::cp;
 Byte _numberofnetworks = 0;
 Int _c = 0;
-B4R::B4RString be_ann103_8;
-B4R::B4RString be_ann105_4;
-B4R::B4RString be_ann105_6;
-B4R::B4RString be_ann107_8;
-B4R::B4RString be_ann109_4;
-B4R::B4RString be_ann109_6;
- //BA.debugLineNum = 135;BA.debugLine="Sub ConnectToWifi";
- //BA.debugLineNum = 136;BA.debugLine="Dim numberOfNetworks As Byte = WiFi.Scan";
+B4R::B4RString be_ann106_8;
+B4R::B4RString be_ann108_4;
+B4R::B4RString be_ann108_6;
+B4R::B4RString be_ann110_8;
+B4R::B4RString be_ann112_4;
+B4R::B4RString be_ann112_6;
+ //BA.debugLineNum = 138;BA.debugLine="Sub ConnectToWifi";
+ //BA.debugLineNum = 139;BA.debugLine="Dim numberOfNetworks As Byte = WiFi.Scan";
 _numberofnetworks = b4r_main::_wifi->Scan();
- //BA.debugLineNum = 137;BA.debugLine="Dim c As Int";
+ //BA.debugLineNum = 140;BA.debugLine="Dim c As Int";
 _c = 0;
- //BA.debugLineNum = 138;BA.debugLine="Log(\"Found \", numberOfNetworks, \" networks\")";
+ //BA.debugLineNum = 141;BA.debugLine="Log(\"Found \", numberOfNetworks, \" networks\")";
 B4R::Common::LogHelper(3,102,F("Found "),1,_numberofnetworks,102,F(" networks"));
- //BA.debugLineNum = 139;BA.debugLine="For c = 0 To numberOfNetworks - 1";
+ //BA.debugLineNum = 142;BA.debugLine="For c = 0 To numberOfNetworks - 1";
 {
 const int step4 = 1;
 const int limit4 = (Int) (_numberofnetworks-1);
 _c = 0 ;
 for (;(step4 > 0 && _c <= limit4) || (step4 < 0 && _c >= limit4) ;_c = ((Int)(0 + _c + step4))  ) {
- //BA.debugLineNum = 140;BA.debugLine="Log(\"SSID: \", WiFi.ScannedSSID(c), \"|RSSI: \", Wi";
+ //BA.debugLineNum = 143;BA.debugLine="Log(\"SSID: \", WiFi.ScannedSSID(c), \"|RSSI: \", Wi";
 B4R::Common::LogHelper(4,102,F("SSID: "),101,b4r_main::_wifi->ScannedSSID((Byte) (_c))->data,102,F("|RSSI: "),5,b4r_main::_wifi->ScannedRSSI((Byte) (_c)));
- //BA.debugLineNum = 141;BA.debugLine="If WiFi.ScannedSSID(c) = \"CloydNote8\" Then";
-if ((b4r_main::_wifi->ScannedSSID((Byte) (_c)))->equals(be_ann103_8.wrap("CloydNote8"))) { 
- //BA.debugLineNum = 142;BA.debugLine="Log(\"Connecting to CloydNote8 WiFi\")";
+ //BA.debugLineNum = 144;BA.debugLine="If WiFi.ScannedSSID(c) = \"CloydNote8\" Then";
+if ((b4r_main::_wifi->ScannedSSID((Byte) (_c)))->equals(be_ann106_8.wrap("CloydNote8"))) { 
+ //BA.debugLineNum = 145;BA.debugLine="Log(\"Connecting to CloydNote8 WiFi\")";
 B4R::Common::LogHelper(1,102,F("Connecting to CloydNote8 WiFi"));
- //BA.debugLineNum = 143;BA.debugLine="WiFi.Connect2(\"CloydNote8\", \"adjj6347\")";
-b4r_main::_wifi->Connect2(be_ann105_4.wrap("CloydNote8"),be_ann105_6.wrap("adjj6347"));
- //BA.debugLineNum = 144;BA.debugLine="Exit";
+ //BA.debugLineNum = 146;BA.debugLine="WiFi.Connect2(\"CloydNote8\", \"adjj6347\")";
+b4r_main::_wifi->Connect2(be_ann108_4.wrap("CloydNote8"),be_ann108_6.wrap("adjj6347"));
+ //BA.debugLineNum = 147;BA.debugLine="Exit";
 if (true) break;
- }else if((b4r_main::_wifi->ScannedSSID((Byte) (_c)))->equals(be_ann107_8.wrap("Rise Above This Home"))) { 
- //BA.debugLineNum = 146;BA.debugLine="Log(\"Connecting to Rise Above This Home WiFi\")";
+ }else if((b4r_main::_wifi->ScannedSSID((Byte) (_c)))->equals(be_ann110_8.wrap("Rise Above This Home"))) { 
+ //BA.debugLineNum = 149;BA.debugLine="Log(\"Connecting to Rise Above This Home WiFi\")";
 B4R::Common::LogHelper(1,102,F("Connecting to Rise Above This Home WiFi"));
- //BA.debugLineNum = 147;BA.debugLine="WiFi.Connect2(\"Rise Above This Home\", \"SteelRes";
-b4r_main::_wifi->Connect2(be_ann109_4.wrap("Rise Above This Home"),be_ann109_6.wrap("SteelReserve"));
- //BA.debugLineNum = 148;BA.debugLine="Exit";
+ //BA.debugLineNum = 150;BA.debugLine="WiFi.Connect2(\"Rise Above This Home\", \"SteelRes";
+b4r_main::_wifi->Connect2(be_ann112_4.wrap("Rise Above This Home"),be_ann112_6.wrap("SteelReserve"));
+ //BA.debugLineNum = 151;BA.debugLine="Exit";
 if (true) break;
  }else {
  };
  }
 };
- //BA.debugLineNum = 153;BA.debugLine="If WiFi.IsConnected Then";
+ //BA.debugLineNum = 156;BA.debugLine="If WiFi.IsConnected Then";
 if (b4r_main::_wifi->getIsConnected()) { 
- //BA.debugLineNum = 154;BA.debugLine="Log(\"Connected to WiFi, Local IP \", WiFi.LocalIp";
+ //BA.debugLineNum = 157;BA.debugLine="Log(\"Connected to WiFi, Local IP \", WiFi.LocalIp";
 B4R::Common::LogHelper(2,102,F("Connected to WiFi, Local IP "),101,b4r_main::_wifi->getLocalIp()->data);
  }else {
- //BA.debugLineNum = 156;BA.debugLine="Log(\"Not Connected to WiFi\")";
+ //BA.debugLineNum = 159;BA.debugLine="Log(\"Not Connected to WiFi\")";
 B4R::Common::LogHelper(1,102,F("Not Connected to WiFi"));
  };
- //BA.debugLineNum = 158;BA.debugLine="End Sub";
+ //BA.debugLineNum = 161;BA.debugLine="End Sub";
 B4R::StackMemory::cp = cp;
 }
 void b4r_main::_mqtt_connect(Byte _unused){
 const UInt cp = B4R::StackMemory::cp;
-B4R::B4RString be_ann37_4;
+B4R::B4RString be_ann40_4;
  //BA.debugLineNum = 62;BA.debugLine="Sub MQTT_Connect(Unused As Byte)";
- //BA.debugLineNum = 63;BA.debugLine="If MQTT.Connect = False Then";
+ //BA.debugLineNum = 63;BA.debugLine="If WiFi.IsConnected = False Then";
+if (b4r_main::_wifi->getIsConnected()==Common_False) { 
+ //BA.debugLineNum = 64;BA.debugLine="ConnectToWifi";
+_connecttowifi();
+ };
+ //BA.debugLineNum = 66;BA.debugLine="If MQTT.Connect = False Then";
 if (b4r_main::_mqtt->Connect()==Common_False) { 
- //BA.debugLineNum = 64;BA.debugLine="Log(\"Connecting to broker\")";
+ //BA.debugLineNum = 67;BA.debugLine="Log(\"Connecting to broker\")";
 B4R::Common::LogHelper(1,102,F("Connecting to broker"));
- //BA.debugLineNum = 65;BA.debugLine="MQTT.Connect2(MQTTOpt)";
+ //BA.debugLineNum = 68;BA.debugLine="MQTT.Connect2(MQTTOpt)";
 b4r_main::_mqtt->Connect2(b4r_main::_mqttopt);
- //BA.debugLineNum = 66;BA.debugLine="CallSubPlus(\"MQTT_Connect\", 1000, 0)";
+ //BA.debugLineNum = 69;BA.debugLine="CallSubPlus(\"MQTT_Connect\", 1000, 0)";
 B4R::__c->CallSubPlus(_mqtt_connect,(ULong) (1000),(Byte) (0));
  }else {
- //BA.debugLineNum = 68;BA.debugLine="pin16.DigitalWrite(False)";
+ //BA.debugLineNum = 71;BA.debugLine="pin16.DigitalWrite(False)";
 b4r_main::_pin16->DigitalWrite(Common_False);
- //BA.debugLineNum = 69;BA.debugLine="Log(\"Connected to broker\")";
+ //BA.debugLineNum = 72;BA.debugLine="Log(\"Connected to broker\")";
 B4R::Common::LogHelper(1,102,F("Connected to broker"));
- //BA.debugLineNum = 70;BA.debugLine="MQTT.Subscribe(\"Andy\", 0)";
-b4r_main::_mqtt->Subscribe(be_ann37_4.wrap("Andy"),(Byte) (0));
+ //BA.debugLineNum = 73;BA.debugLine="MQTT.Subscribe(\"Andy\", 0)";
+b4r_main::_mqtt->Subscribe(be_ann40_4.wrap("Andy"),(Byte) (0));
  };
- //BA.debugLineNum = 72;BA.debugLine="End Sub";
+ //BA.debugLineNum = 75;BA.debugLine="End Sub";
 B4R::StackMemory::cp = cp;
 }
 void b4r_main::_mqtt_disconnected(){
 const UInt cp = B4R::StackMemory::cp;
- //BA.debugLineNum = 125;BA.debugLine="Sub mqtt_Disconnected";
- //BA.debugLineNum = 126;BA.debugLine="pin16.DigitalWrite(True)";
+ //BA.debugLineNum = 128;BA.debugLine="Sub mqtt_Disconnected";
+ //BA.debugLineNum = 129;BA.debugLine="pin16.DigitalWrite(True)";
 b4r_main::_pin16->DigitalWrite(Common_True);
- //BA.debugLineNum = 127;BA.debugLine="Log(\"Disconnected from broker\")";
+ //BA.debugLineNum = 130;BA.debugLine="Log(\"Disconnected from broker\")";
 B4R::Common::LogHelper(1,102,F("Disconnected from broker"));
- //BA.debugLineNum = 128;BA.debugLine="MQTT.Close";
+ //BA.debugLineNum = 131;BA.debugLine="MQTT.Close";
 b4r_main::_mqtt->Close();
- //BA.debugLineNum = 129;BA.debugLine="If WiFi.IsConnected = False Then";
+ //BA.debugLineNum = 132;BA.debugLine="If WiFi.IsConnected = False Then";
 if (b4r_main::_wifi->getIsConnected()==Common_False) { 
- //BA.debugLineNum = 130;BA.debugLine="ConnectToWifi";
+ //BA.debugLineNum = 133;BA.debugLine="ConnectToWifi";
 _connecttowifi();
  };
- //BA.debugLineNum = 132;BA.debugLine="MQTT_Connect(0)";
+ //BA.debugLineNum = 135;BA.debugLine="MQTT_Connect(0)";
 _mqtt_connect((Byte) (0));
- //BA.debugLineNum = 133;BA.debugLine="End Sub";
+ //BA.debugLineNum = 136;BA.debugLine="End Sub";
 B4R::StackMemory::cp = cp;
 }
 void b4r_main::_mqtt_messagearrived(B4R::B4RString* _topic,B4R::Array* _payload){
 const UInt cp = B4R::StackMemory::cp;
-B4R::Object be_ann66_8;
-B4R::B4RString be_ann67_3;
-B4R::B4RString be_ann68_4;
-B4R::B4RString be_ann68_6;
-B4R::B4RString be_ann71_3;
-B4R::B4RString be_ann72_4;
-B4R::B4RString be_ann72_6;
-B4R::B4RString be_ann75_3;
-B4R::B4RString be_ann76_4;
-B4R::B4RString be_ann76_6;
+B4R::Object be_ann69_8;
+B4R::B4RString be_ann70_3;
+B4R::B4RString be_ann71_4;
+B4R::B4RString be_ann71_6;
+B4R::B4RString be_ann74_3;
+B4R::B4RString be_ann75_4;
+B4R::B4RString be_ann75_6;
 B4R::B4RString be_ann78_3;
-B4R::B4RString be_ann80_4;
-B4R::B4RString be_ann80_6;
-B4R::B4RString be_ann82_4;
-B4R::B4RString be_ann82_6;
-B4R::B4RString be_ann84_4;
-B4R::B4RString be_ann84_6;
- //BA.debugLineNum = 100;BA.debugLine="Sub mqtt_MessageArrived (Topic As String, Payload(";
- //BA.debugLineNum = 101;BA.debugLine="pin16.DigitalWrite(False)";
+B4R::B4RString be_ann79_4;
+B4R::B4RString be_ann79_6;
+B4R::B4RString be_ann81_3;
+B4R::B4RString be_ann83_4;
+B4R::B4RString be_ann83_6;
+B4R::B4RString be_ann85_4;
+B4R::B4RString be_ann85_6;
+B4R::B4RString be_ann87_4;
+B4R::B4RString be_ann87_6;
+ //BA.debugLineNum = 103;BA.debugLine="Sub mqtt_MessageArrived (Topic As String, Payload(";
+ //BA.debugLineNum = 104;BA.debugLine="pin16.DigitalWrite(False)";
 b4r_main::_pin16->DigitalWrite(Common_False);
- //BA.debugLineNum = 102;BA.debugLine="Log(\"Message arrived. Topic=\", Topic, \" Payload=\"";
-B4R::Common::LogHelper(4,102,F("Message arrived. Topic="),101,_topic->data,102,F(" Payload="),100,be_ann66_8.wrapPointer(_payload));
- //BA.debugLineNum = 103;BA.debugLine="If Payload=\"Remotely close relay\" Then";
-if ((_payload)->equals((be_ann67_3.wrap("Remotely close relay"))->GetBytes())) { 
- //BA.debugLineNum = 104;BA.debugLine="MQTT.Publish(\"Andy\",\"*Closing relay by remote\")";
-b4r_main::_mqtt->Publish(be_ann68_4.wrap("Andy"),(be_ann68_6.wrap("*Closing relay by remote"))->GetBytes());
- //BA.debugLineNum = 105;BA.debugLine="d6.DigitalWrite(True)";
+ //BA.debugLineNum = 105;BA.debugLine="Log(\"Message arrived. Topic=\", Topic, \" Payload=\"";
+B4R::Common::LogHelper(4,102,F("Message arrived. Topic="),101,_topic->data,102,F(" Payload="),100,be_ann69_8.wrapPointer(_payload));
+ //BA.debugLineNum = 106;BA.debugLine="If Payload=\"Remotely close relay\" Then";
+if ((_payload)->equals((be_ann70_3.wrap("Remotely close relay"))->GetBytes())) { 
+ //BA.debugLineNum = 107;BA.debugLine="MQTT.Publish(\"Andy\",\"*Closing relay by remote\")";
+b4r_main::_mqtt->Publish(be_ann71_4.wrap("Andy"),(be_ann71_6.wrap("*Closing relay by remote"))->GetBytes());
+ //BA.debugLineNum = 108;BA.debugLine="d6.DigitalWrite(True)";
 b4r_main::_d6->DigitalWrite(Common_True);
- //BA.debugLineNum = 106;BA.debugLine="StopRelay = True";
+ //BA.debugLineNum = 109;BA.debugLine="StopRelay = True";
 b4r_main::_stoprelay = Common_True;
- }else if((_payload)->equals((be_ann71_3.wrap("Remotely open relay"))->GetBytes())) { 
- //BA.debugLineNum = 108;BA.debugLine="MQTT.Publish(\"Andy\",\"*Opening relay by remote\")";
-b4r_main::_mqtt->Publish(be_ann72_4.wrap("Andy"),(be_ann72_6.wrap("*Opening relay by remote"))->GetBytes());
- //BA.debugLineNum = 109;BA.debugLine="StopRelay = False";
+ }else if((_payload)->equals((be_ann74_3.wrap("Remotely open relay"))->GetBytes())) { 
+ //BA.debugLineNum = 111;BA.debugLine="MQTT.Publish(\"Andy\",\"*Opening relay by remote\")";
+b4r_main::_mqtt->Publish(be_ann75_4.wrap("Andy"),(be_ann75_6.wrap("*Opening relay by remote"))->GetBytes());
+ //BA.debugLineNum = 112;BA.debugLine="StopRelay = False";
 b4r_main::_stoprelay = Common_False;
- //BA.debugLineNum = 110;BA.debugLine="RelayOpen(0)";
+ //BA.debugLineNum = 113;BA.debugLine="RelayOpen(0)";
 _relayopen((Byte) (0));
- }else if((_payload)->equals((be_ann75_3.wrap("Restart controller"))->GetBytes())) { 
- //BA.debugLineNum = 112;BA.debugLine="MQTT.Publish(\"Andy\",\"*Restarting relay by remote";
-b4r_main::_mqtt->Publish(be_ann76_4.wrap("Andy"),(be_ann76_6.wrap("*Restarting relay by remote"))->GetBytes());
- //BA.debugLineNum = 113;BA.debugLine="ESP.Restart";
+ }else if((_payload)->equals((be_ann78_3.wrap("Restart controller"))->GetBytes())) { 
+ //BA.debugLineNum = 115;BA.debugLine="MQTT.Publish(\"Andy\",\"*Restarting relay by remote";
+b4r_main::_mqtt->Publish(be_ann79_4.wrap("Andy"),(be_ann79_6.wrap("*Restarting relay by remote"))->GetBytes());
+ //BA.debugLineNum = 116;BA.debugLine="ESP.Restart";
 b4r_main::_esp->Restart();
- }else if((_payload)->equals((be_ann78_3.wrap("Check relay status"))->GetBytes())) { 
- //BA.debugLineNum = 115;BA.debugLine="If StopRelay Then";
+ }else if((_payload)->equals((be_ann81_3.wrap("Check relay status"))->GetBytes())) { 
+ //BA.debugLineNum = 118;BA.debugLine="If StopRelay Then";
 if (b4r_main::_stoprelay) { 
- //BA.debugLineNum = 116;BA.debugLine="MQTT.Publish(\"Andy\",\"*Relay is permanently clos";
-b4r_main::_mqtt->Publish(be_ann80_4.wrap("Andy"),(be_ann80_6.wrap("*Relay is permanently closed"))->GetBytes());
+ //BA.debugLineNum = 119;BA.debugLine="MQTT.Publish(\"Andy\",\"*Relay is permanently clos";
+b4r_main::_mqtt->Publish(be_ann83_4.wrap("Andy"),(be_ann83_6.wrap("*Relay is permanently closed"))->GetBytes());
  }else if(b4r_main::_d6->DigitalRead()) { 
- //BA.debugLineNum = 118;BA.debugLine="MQTT.Publish(\"Andy\",\"*Relay is closed\")";
-b4r_main::_mqtt->Publish(be_ann82_4.wrap("Andy"),(be_ann82_6.wrap("*Relay is closed"))->GetBytes());
+ //BA.debugLineNum = 121;BA.debugLine="MQTT.Publish(\"Andy\",\"*Relay is closed\")";
+b4r_main::_mqtt->Publish(be_ann85_4.wrap("Andy"),(be_ann85_6.wrap("*Relay is closed"))->GetBytes());
  }else {
- //BA.debugLineNum = 120;BA.debugLine="MQTT.Publish(\"Andy\",\"*Relay is open\")";
-b4r_main::_mqtt->Publish(be_ann84_4.wrap("Andy"),(be_ann84_6.wrap("*Relay is open"))->GetBytes());
+ //BA.debugLineNum = 123;BA.debugLine="MQTT.Publish(\"Andy\",\"*Relay is open\")";
+b4r_main::_mqtt->Publish(be_ann87_4.wrap("Andy"),(be_ann87_6.wrap("*Relay is open"))->GetBytes());
  };
  };
- //BA.debugLineNum = 123;BA.debugLine="End Sub";
+ //BA.debugLineNum = 126;BA.debugLine="End Sub";
 B4R::StackMemory::cp = cp;
 }
 
@@ -256,55 +261,55 @@ b4r_main::_esp = &be_gann14_3;
 }
 void b4r_main::_relayclose(Byte _tag){
 const UInt cp = B4R::StackMemory::cp;
-B4R::B4RString be_ann48_4;
-B4R::B4RString be_ann48_6;
- //BA.debugLineNum = 74;BA.debugLine="Private Sub RelayClose (tag As Byte)";
- //BA.debugLineNum = 75;BA.debugLine="If StopRelay Then";
-if (b4r_main::_stoprelay) { 
- //BA.debugLineNum = 76;BA.debugLine="Return";
-B4R::StackMemory::cp = cp;
-if (true) return ;
- };
+B4R::B4RString be_ann51_4;
+B4R::B4RString be_ann51_6;
+ //BA.debugLineNum = 77;BA.debugLine="Private Sub RelayClose (tag As Byte)";
  //BA.debugLineNum = 78;BA.debugLine="If WiFi.IsConnected = False Then";
 if (b4r_main::_wifi->getIsConnected()==Common_False) { 
  //BA.debugLineNum = 79;BA.debugLine="ConnectToWifi";
 _connecttowifi();
  };
- //BA.debugLineNum = 81;BA.debugLine="Log(\"*Relay has been closed\")";
+ //BA.debugLineNum = 81;BA.debugLine="If StopRelay Then";
+if (b4r_main::_stoprelay) { 
+ //BA.debugLineNum = 82;BA.debugLine="Return";
+B4R::StackMemory::cp = cp;
+if (true) return ;
+ };
+ //BA.debugLineNum = 84;BA.debugLine="Log(\"*Relay has been closed\")";
 B4R::Common::LogHelper(1,102,F("*Relay has been closed"));
- //BA.debugLineNum = 82;BA.debugLine="MQTT.Publish(\"Andy\",\"*Relay has been closed\")";
-b4r_main::_mqtt->Publish(be_ann48_4.wrap("Andy"),(be_ann48_6.wrap("*Relay has been closed"))->GetBytes());
- //BA.debugLineNum = 83;BA.debugLine="d6.DigitalWrite(True)";
+ //BA.debugLineNum = 85;BA.debugLine="MQTT.Publish(\"Andy\",\"*Relay has been closed\")";
+b4r_main::_mqtt->Publish(be_ann51_4.wrap("Andy"),(be_ann51_6.wrap("*Relay has been closed"))->GetBytes());
+ //BA.debugLineNum = 86;BA.debugLine="d6.DigitalWrite(True)";
 b4r_main::_d6->DigitalWrite(Common_True);
- //BA.debugLineNum = 84;BA.debugLine="CallSubPlus(\"RelayOpen\", 15000, 0)";
+ //BA.debugLineNum = 87;BA.debugLine="CallSubPlus(\"RelayOpen\", 15000, 0)";
 B4R::__c->CallSubPlus(_relayopen,(ULong) (15000),(Byte) (0));
- //BA.debugLineNum = 85;BA.debugLine="End Sub";
+ //BA.debugLineNum = 88;BA.debugLine="End Sub";
 B4R::StackMemory::cp = cp;
 }
 void b4r_main::_relayopen(Byte _tag){
 const UInt cp = B4R::StackMemory::cp;
-B4R::B4RString be_ann60_4;
-B4R::B4RString be_ann60_6;
- //BA.debugLineNum = 87;BA.debugLine="Private Sub RelayOpen (tag As Byte)";
- //BA.debugLineNum = 88;BA.debugLine="If StopRelay Then";
-if (b4r_main::_stoprelay) { 
- //BA.debugLineNum = 89;BA.debugLine="Return";
-B4R::StackMemory::cp = cp;
-if (true) return ;
- };
+B4R::B4RString be_ann63_4;
+B4R::B4RString be_ann63_6;
+ //BA.debugLineNum = 90;BA.debugLine="Private Sub RelayOpen (tag As Byte)";
  //BA.debugLineNum = 91;BA.debugLine="If WiFi.IsConnected = False Then";
 if (b4r_main::_wifi->getIsConnected()==Common_False) { 
  //BA.debugLineNum = 92;BA.debugLine="ConnectToWifi";
 _connecttowifi();
  };
- //BA.debugLineNum = 94;BA.debugLine="Log(\"*Relay has been opened\")";
+ //BA.debugLineNum = 94;BA.debugLine="If StopRelay Then";
+if (b4r_main::_stoprelay) { 
+ //BA.debugLineNum = 95;BA.debugLine="Return";
+B4R::StackMemory::cp = cp;
+if (true) return ;
+ };
+ //BA.debugLineNum = 97;BA.debugLine="Log(\"*Relay has been opened\")";
 B4R::Common::LogHelper(1,102,F("*Relay has been opened"));
- //BA.debugLineNum = 95;BA.debugLine="MQTT.Publish(\"Andy\",\"*Relay has been opened\")";
-b4r_main::_mqtt->Publish(be_ann60_4.wrap("Andy"),(be_ann60_6.wrap("*Relay has been opened"))->GetBytes());
- //BA.debugLineNum = 96;BA.debugLine="d6.DigitalWrite(False)";
+ //BA.debugLineNum = 98;BA.debugLine="MQTT.Publish(\"Andy\",\"*Relay has been opened\")";
+b4r_main::_mqtt->Publish(be_ann63_4.wrap("Andy"),(be_ann63_6.wrap("*Relay has been opened"))->GetBytes());
+ //BA.debugLineNum = 99;BA.debugLine="d6.DigitalWrite(False)";
 b4r_main::_d6->DigitalWrite(Common_False);
- //BA.debugLineNum = 97;BA.debugLine="CallSubPlus(\"RelayClose\", 5000, 0)";
+ //BA.debugLineNum = 100;BA.debugLine="CallSubPlus(\"RelayClose\", 5000, 0)";
 B4R::__c->CallSubPlus(_relayclose,(ULong) (5000),(Byte) (0));
- //BA.debugLineNum = 98;BA.debugLine="End Sub";
+ //BA.debugLineNum = 101;BA.debugLine="End Sub";
 B4R::StackMemory::cp = cp;
 }
--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # ESP8266RelayControl
-This sketch is written for our Technical Support director's home automation.
+These ESP8266 program and Android application client are written for our Technical Support director's home automation. The mobile application remotely controls and monitors the relay via cloud MQTT.
 
 Hardware used for Internet of Things (IoT):
 1. NodeMcu LUA WIFI Board Based on ESP8266 CP2102 Module.
 2. Tongling JQC-3FF-S-Z 3.3v/5v relay.
+
+Mobile application used for Internet of Things (IoT):
+1. Custom-made Android mobile application.
